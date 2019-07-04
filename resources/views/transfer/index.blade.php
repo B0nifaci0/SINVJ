b1f342d451d96d559d98443d18ff74740e264af5
Juliobg98 <juliopmcanal5@gmail.com>
2019-07-04 15:50:43 -0500

    Select Search

--- resources/views/transfer/index.blade.php
+++ resources/views/transfer/index.blade.php
@@ -38,67 +38,82 @@ Traspasos
           </ul>
         </div>
     @endif
-      <h2 align="center">Transpasos </h2>
+      <h2 align="center">Traspasos </h2>
       <br>  
       <form class="" action="/transpasos" method="POST" enctype="multipart/form-data">
       {{ csrf_field() }} 
       <div class='row'>
-      
-      <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
-               <label>Producto</label>
-               <select name="product_id" class="form-control round">
-               
-                 @foreach($products as $product)
-                  <option value="{{ $product->id }}" required>{{$product->id}}-{{ $product->name }}</option>
-                 @endforeach
-               </select>
-            </div>
-            <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
-               <label>Sucursal</label>
-               <select name="branch_id" class="form-control round">
-                 @foreach($branches as $branch)
-                  <option value="{{ $branch->id }}" required>{{ $branch->name }}</option>
-                 @endforeach
-               </select>
-            </div>
-            <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
-               <label>Colaborador</label>
-               <select name="user_id" class="form-control round">
-               
-                 @foreach($users as $user)
-                  <option value="{{ $user->id }}" required>{{ $user->name }}</option>
-                 @endforeach
-               </select>
-            </div>
+               <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
+                  <label>Producto</label>
+                  <select name="product_id" class="form-control" data-plugin="select2" data-placeholder="Seleccione Producto"
+                      data-allow-clear="true">
+                      <option></option>
+                      <optgroup label="Productos">
+                      @foreach($products as $product)
+                     <option value="{{ $product->id }}" required>{{$product->id}}-{{ $product->name }}</option>
+                     @endforeach
+                      </optgroup>
+                   </select>
+               </div>
+               <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
+                  <label>Sucursal</label>
+                  <select name="branch_id" class="form-control ">
+                    @foreach($branches as $branch)
+                     <option value="{{ $branch->id }}" required>{{ $branch->name }}</option>
+                    @endforeach
+                  </select>
+               </div>
+              <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
+                 <label>Colaborador</label>
+                 <select name="user_id" class="form-control ">
+                   @foreach($users as $user)
+                    <option value="{{ $user->id }}" required>{{ $user->name }}</option>
+                   @endforeach
+                 </select>
+              </div>
+              <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
+                 <label>Destino</label>
+                   <select name="branch_id" class="form-control ">
+                      @foreach($branches as $branch)
+                       <option value="{{ $branch->id }}" required>{{ $branch->name }}</option>
+                      @endforeach
+                   </select>
+               </div>
 
-            <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
-            <label>Destino</label>
-               <select name="branch_id" class="form-control round">
-               
-                 @foreach($branches as $branch)
-                  <option value="{{ $branch->id }}" required>{{ $branch->name }}</option>
-                 @endforeach
-               </select>
-            </div>
-
-            <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
-               <label>Colaborador</label>
-               <select name="user_id" class="form-control round">
-               
-                 @foreach($users as $user)
-                  <option value="{{ $user->id }}" required>{{ $user->name }}</option>
-                 @endforeach
-               </select>
-            </div>
-            <br>
-      </div>
-      <br>
+               <div class="col-md-3">
+                 <label  class="control-label">Seleccione Sucursal</label>
+                  <select id="branch_id"   name="branch_id"  class="form-control ">
+                    @foreach($branches as $branch)            
+                      <option value="{{ $branch->id }}" required>{{ $branch->name }}</option>
+                    @endforeach
+                  </select> 
+               </div> 
+               <div class="col-md-3  col-md-offset-1 visible-md visible-lg">
+                 <label>Colaborador</label>
+                 <select name="" id="user_id" class="form-control ">
+                   @foreach($users as $user)
+                   <option value="{{ $user->id }}" required>{{ $user->name }}</option>
+                   @endforeach
+                 </select>
+               </div>
+                <br>
+              </div>
+             <br>
             <div class="form-group col-md-12">
           <button id="submit" type="submit" name="button" class="btn btn-primary">Guardar</button>
-        </div>
-
-            @endsection
+       </div>
+@endsection
         
+@section('branch-user')
+<script type="text/javascript">
 
+var branches = {!! $branches !!};
 
+$('#branch_id').change(function() {
+  var id = $(this).val();
+  var branch = branches.filter(l => l.id == id)[0];
+  $('#user_id').val(user.id);
+});
 
+</script>
+@endsection
