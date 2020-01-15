821a6bd0ea65a8107fe0798e32c3cda70edcf518
Pedro <pether.1994@gmail.com>
2020-01-15 12:44:04 -0600

    nombre

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,17 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta name="description" content="bootstrap material admin template">
     <meta name="author" content="">
-    
+
     <title>Login SINVJ | Admin Template</title>
-    
+
     <link rel="apple-touch-icon" href="../../assets/images/apple-touch-icon.png">
     <link rel="shortcut icon" href="../../assets/images/favicon.ico">
-    
+
     <!-- Stylesheets -->
     <link rel="stylesheet" href={{{url('global/css/bootstrap.min.css')}}}>
     <link rel="stylesheet" href={{{url('global/css/bootstrap-extend.min.css')}}}>
     <link rel="stylesheet" href={{{url('assets/css/site.min.css')}}}>
-    
+
     <!-- Plugins -->
     <link rel="stylesheet" href={{{url('global/vendor/animsition/animsition.css')}}}>
     <link rel="stylesheet" href={{{url('global/vendor/asscrollable/asScrollable.css')}}}>
@@ -26,13 +26,13 @@
     <link rel="stylesheet" href={{{url('global/vendor/flag-icon-css/flag-icon.css')}}}>
     <link rel="stylesheet" href={{{url('global/vendor/waves/waves.css')}}}>
     <link rel="stylesheet" href={{{url('assets/examples/css/pages/login-v2.css')}}}>
-    
-    
+
+
     <!-- Fonts -->
     <link rel="stylesheet" href={{{url('global/fonts/material-design/material-design.min.css')}}}>
     <link rel="stylesheet" href={{{url('global/fonts/brand-icons/brand-icons.min.css')}}}>
     <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,300italic'>
-     
+
     <!-- Scripts -->
     <script src={{{url('global/vendor/breakpoints/breakpoints.js')}}}></script>
     <script>
@@ -47,7 +47,7 @@
         <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'SINVJ') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -110,30 +110,30 @@
     <script src={{{url('global/vendor/asscrollbar/jquery-asScrollbar.js')}}}></script>
     <script src={{{url('global/vendor/asscrollable/jquery-asScrollable.js')}}}></script>
     <script src={{{url('global/vendor/waves/waves.js')}}}></script>
-    
+
     <!-- Plugins -->
     <script src={{{url('global/vendor/switchery/switchery.js')}}}></script>
     <script src={{{url('global/vendor/intro-js/intro.js')}}}></script>
     <script src={{{url('global/vendor/screenfull/screenfull.js')}}}></script>
     <script src={{{url('global/vendor/slidepanel/jquery-slidePanel.js')}}}></script>
         <script src={{{url('global/vendor/jquery-placeholder/jquery.placeholder.js')}}}></script>
-    
+
     <!-- Scripts -->
     <script src={{{url('global/js/Component.js')}}}></script>
     <script src={{{url('global/js/Plugin.js')}}}></script>
     <script src={{{url('global/js/Base.js')}}}></script>
     <script src={{{url('global/js/Config.js')}}}></script>
-    
+
     <script src={{{url('assets/js/Section/Menubar.js')}}}></script>
     <script src={{{url('assets/js/Section/Sidebar.js')}}}></script>
     <script src={{{url('assets/js/Section/PageAside.js')}}}></script>
     <script src={{{url('assets/js/Plugin/menu.js')}}}></script>
-    
+
     <!-- Config -->
     <script src={{{url('global/js/config/colors.js')}}}></script>
     <script src={{{url('assets/js/config/tour.js')}}}></script>
     <script>Config.set('assets', '../../assets');</script>
-    
+
     <!-- Page -->
     <script src={{{url('assets/js/Site.js')}}}></script>
     <script src={{{url('global/js/Plugin/asscrollable.js')}}}></script>
@@ -141,11 +141,11 @@
     <script src={{{url('global/js/Plugin/switchery.js')}}}></script>
         <script src={{{url('global/js/Plugin/jquery-placeholder.js')}}}></script>
         <script src={{{url('global/js/Plugin/material.js')}}}></script>
-    
+
     <script>
       (function(document, window, $){
         'use strict';
-    
+
         var Site = window.Site;
         $(document).ready(function(){
           Site.run();
