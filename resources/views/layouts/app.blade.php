04da9c656680edad4ea86dc0e9ba99bdb777034d
Pedro <pether.1994@gmail.com>
2020-01-15 11:56:43 -0600

    correcion tablas, botones y verificacion por correo

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html class="no-js css-menubar" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="bootstrap material admin template">
+    <meta name="author" content="">
+    
+    <title>Login SINVJ | Admin Template</title>
+    
+    <link rel="apple-touch-icon" href="../../assets/images/apple-touch-icon.png">
+    <link rel="shortcut icon" href="../../assets/images/favicon.ico">
+    
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href={{{url('global/css/bootstrap.min.css')}}}>
+    <link rel="stylesheet" href={{{url('global/css/bootstrap-extend.min.css')}}}>
+    <link rel="stylesheet" href={{{url('assets/css/site.min.css')}}}>
+    
+    <!-- Plugins -->
+    <link rel="stylesheet" href={{{url('global/vendor/animsition/animsition.css')}}}>
+    <link rel="stylesheet" href={{{url('global/vendor/asscrollable/asScrollable.css')}}}>
+    <link rel="stylesheet" href={{{url('global/vendor/switchery/switchery.css')}}}>
+    <link rel="stylesheet" href={{{url('global/vendor/intro-js/introjs.css')}}}>
+    <link rel="stylesheet" href={{{url('global/vendor/slidepanel/slidePanel.css')}}}>
+    <link rel="stylesheet" href={{{url('global/vendor/flag-icon-css/flag-icon.css')}}}>
+    <link rel="stylesheet" href={{{url('global/vendor/waves/waves.css')}}}>
+    <link rel="stylesheet" href={{{url('assets/examples/css/pages/login-v2.css')}}}>
+    
+    
+    <!-- Fonts -->
+    <link rel="stylesheet" href={{{url('global/fonts/material-design/material-design.min.css')}}}>
+    <link rel="stylesheet" href={{{url('global/fonts/brand-icons/brand-icons.min.css')}}}>
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,300italic'>
+     
+    <!-- Scripts -->
+    <script src={{{url('global/vendor/breakpoints/breakpoints.js')}}}></script>
+    <script>
+      Breakpoints();
+    </script>
+  </head>
+  <body class="animsition page-login-v2 layout-full page-dark">
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle bg-transparent" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- Core  -->
+    <script src={{{url('global/vendor/babel-external-helpers/babel-external-helpers.js')}}}></script>
+    <script src={{{url('global/vendor/jquery/jquery.js')}}}></script>
+    <script src={{{url('global/vendor/popper-js/umd/popper.min.js')}}}></script>
+    <script src={{{url('global/vendor/bootstrap/bootstrap.js')}}}></script>
+    <script src={{{url('global/vendor/animsition/animsition.js')}}}></script>
+    <script src={{{url('global/vendor/mousewheel/jquery.mousewheel.js')}}}></script>
+    <script src={{{url('global/vendor/asscrollbar/jquery-asScrollbar.js')}}}></script>
+    <script src={{{url('global/vendor/asscrollable/jquery-asScrollable.js')}}}></script>
+    <script src={{{url('global/vendor/waves/waves.js')}}}></script>
+    
+    <!-- Plugins -->
+    <script src={{{url('global/vendor/switchery/switchery.js')}}}></script>
+    <script src={{{url('global/vendor/intro-js/intro.js')}}}></script>
+    <script src={{{url('global/vendor/screenfull/screenfull.js')}}}></script>
+    <script src={{{url('global/vendor/slidepanel/jquery-slidePanel.js')}}}></script>
+        <script src={{{url('global/vendor/jquery-placeholder/jquery.placeholder.js')}}}></script>
+    
+    <!-- Scripts -->
+    <script src={{{url('global/js/Component.js')}}}></script>
+    <script src={{{url('global/js/Plugin.js')}}}></script>
+    <script src={{{url('global/js/Base.js')}}}></script>
+    <script src={{{url('global/js/Config.js')}}}></script>
+    
+    <script src={{{url('assets/js/Section/Menubar.js')}}}></script>
+    <script src={{{url('assets/js/Section/Sidebar.js')}}}></script>
+    <script src={{{url('assets/js/Section/PageAside.js')}}}></script>
+    <script src={{{url('assets/js/Plugin/menu.js')}}}></script>
+    
+    <!-- Config -->
+    <script src={{{url('global/js/config/colors.js')}}}></script>
+    <script src={{{url('assets/js/config/tour.js')}}}></script>
+    <script>Config.set('assets', '../../assets');</script>
+    
+    <!-- Page -->
+    <script src={{{url('assets/js/Site.js')}}}></script>
+    <script src={{{url('global/js/Plugin/asscrollable.js')}}}></script>
+    <script src={{{url('global/js/Plugin/slidepanel.js')}}}></script>
+    <script src={{{url('global/js/Plugin/switchery.js')}}}></script>
+        <script src={{{url('global/js/Plugin/jquery-placeholder.js')}}}></script>
+        <script src={{{url('global/js/Plugin/material.js')}}}></script>
+    
+    <script>
+      (function(document, window, $){
+        'use strict';
+    
+        var Site = window.Site;
+        $(document).ready(function(){
+          Site.run();
+        });
+      })(document, window, jQuery);
+    </script>
+  </body>
+</html>
+
+
+
+
+
