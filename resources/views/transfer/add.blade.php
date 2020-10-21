fc38d520688fb903ac98e927371537ab3e9fef68
Alejandro Armenta Pavón <alex0430@live.com.mx>
2020-10-20 23:20:30 -0500

    Add Transfer

--- resources/views/transfer/add.blade.php
+++ resources/views/transfer/add.blade.php
@@ -30,130 +30,183 @@ ALTA PRODUCTO
                 </ul>
             </div>
             @endif
-            <center>
-                <h3>Nuevo Traspaso</h3>
-            </center>
-            <form class="" action="/traspasos" method="POST">
+            @if($user->type_user == 1)
+            <h3 class="text-center">Traspaso {{$user->shop->name}}</h3>
+            @else
+            <h3 class="text-center">Traspaso Sucursal {{$user->branch->name}}</h3>
+            @endif
+            <form id="form" method="POST" action="/traspasos">
                 {{ csrf_field() }}
-                <div class='row'>
-                    <!-- Select para Seleccionar producto-->
-                    <div class="form-group form-material  col-md-3  col-md-offset-1 visible-md visible-lg">
-                        <label>Producto</label>
-                        <select id="product" class="form-control " data-plugin="select2"
-                            data-placeholder="Seleccione Producto" data-allow-clear="true" required>
-                            <option></option>
-                            <optgroup label="Productos">
-                                @foreach($products as $product)
-                                <option value="{{ $product->clave }}" required>
-                                    {{$product->clave}}-{{ $product->description }}</option>
-                                @endforeach
-                            </optgroup>
-                        </select>
-                        <input type="text" name="product_id" class="invisible" id="product_id">
-                    </div>
-                    <!-- END Select-->
-                    <!-- Select para Seleccionar Sucursal Destino-->
-                    <div class="form-group  col-md-3  col-md-offset-1 visible-md visible-lg">
-                        <label>Sucursal Destino</label>
-                        <select id="branches" class="form-control sucursales" name="new_branch_id" alt="1" required>
-                            <option value="*">Seleccione Sucursal</option>
-                        </select>
-                    </div>
-                    <!-- END Select-->
-                    <!-- Select para Seleccionar Quien Recibe (Usuario) -->
-                    <div class="form-group  col-md-3 col-md-offset-1 visible-md visible-lg">
-                        <label>Recibe</label>
-                        <select id="usuarios_1" name="destination_user_id" class="form-control " required>
-                        </select>
-                    </div>
-                    <!-- END Select-->
-                    <div class="col-md-3">
-                        <input type="hidden" name="status_product" value="0">
-                    </div>
-                </div>
-                <!-- Botón para guardar traspaso-->
                 <div class="form-group col-md-12">
-                    <button id="submit" type="submit" name="button" class="btn btn-primary">Guardar</button>
+                    <div class='row'>
+                        <div class="form-group form-material  col-md-4  col-md-offset-1 visible-md visible-lg">
+                            <label>Sucursal Destino</label>
+                            <select id="branch" class="form-control " data-plugin="select2"
+                                data-placeholder="Sucursal destino" name="new_branch" data-allow-clear="true" required>
+                                <option></option>
+                                <optgroup label="Sucursales">
+                                    @foreach($branches as $branch)
+                                    <option value="{{ $branch->id }}" required>
+                                        {{$branch->name}}</option>
+                                    @endforeach
+                                </optgroup>
+                            </select>
+                        </div>
+                        <div class="form-group  col-md-4 col-md-offset-1 visible-md visible-lg">
+                            <label class="floating-label" for="inputBranch">{{ __('Usuario Destino') }}</label>
+                            <select id="users" class="form-control" name="destination_user" data-plugin="select2"
+                                data-placeholder="Seleccione Destinatario" data-allow-clear="true" required>
+                                <option> </option required>
+                            </select>
+                        </div>
+                        <div class="form-group  col-md-4  col-md-offset-1 visible-md visible-lg">
+                            <label for="product_id" class="floating-label">Clave del producto</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="product_id">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-primary" type="button" id="search_product"><i
+                                            class="icon fa-search"></i></button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <!-- END Botón-->
+                <div class="form-group col-md-12">
+                    <div class="page-content panel-body container-fluid">
+                        <table id="productsInTranfer" class="table display responsive nowrap" style="width: 100%">
+                            <thead>
+                                <tr>
+                                    <th>Clave</th>
+                                    <th>Descripcion</th>
+                                    <th>Categoria</th>
+                                    @if ($user->type_user == 1)
+                                    <th>Sucursal Origen</th>
+                                    @endif
+                                    <th>Linea</th>
+                                    <th>Peso</th>
+                                    <th>Precio</th>
+                                    <th>Eliminar</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <a id="submit" type="button" name="button" class="btn btn-primary text-white">Guardar</a>
+                </div>
+                <input type="hidden" class="form-control" name="products" id="products" />
             </form>
         </div>
     </div>
 </div>
 @endsection
-<!-- Función para seleccionar solo los usuarios de esa sucursal(Combo dinamico)-->
 @section('branch-user')
 <script type="text/javascript">
     var products = {!! $products !!};
-var branches = {!! $branches !!};
-console.log("PPPP", JSON.stringify(products))
+    var branches = {!! $branches !!};
+    var users = {!! $users !!};
+    var user = {!! $user !!};
+    var selectedProducts = [];
+    var p_t = $('#productsInTranfer').DataTable({
+        language: {
+            url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+        },
+        searching: false
+    });
+    var branch_id
+    $('#branch').val('');
 
-$(".sucursales").change(function(){
-  var selector =  $(this).attr("alt");
-  var id_sucursal = $(this).val();
-  var url = '/sucursales/' + id_sucursal + '/usuarios';
-  $.get(url, function(json){
-    $('#usuarios_' + selector).empty();
-    $('#usuarios_' + selector).append('<option value="" selected disabled>Seleccione un colaborador</option>');
-        $.each(json,function(i, user){
-          $('#usuarios_' + selector).append('<option value = '+ user.id +'>' + user.name +'</option>'
-          );
+    $('#branch').change(function () {
+        branch_id = $(this).val();
+        var url = '/sucursales/' + branch_id + '/usuarios';
+        $.get(url, function (json) {
+            $('#users').empty();
+            $('#users').append('<option value=""></option>');
+            $.each(json, function (i, user) {
+                $('#users').append('<option value=' + user.id + '>' + user.name + '</option>')
+            });
         });
-  });
-});
-
-$(".usuarios").change(function(){
-      var id_user = $(this).val();
-      var selector = $(this).attr("alt");
-      $("#id_user_" + selector).val(id_user);
     });
 
-$(".sucursales1").change(function(){
-  var selector =  $(this).attr("alt");
-  var id_sucursal = $(this).val();
-  var url = '/sucursales/' + id_sucursal + '/usuarios';
-  $.get(url, function(json){
-    $('#usuario_' + selector).empty();
-        $.each(json,function(i, user){
-          $('#usuario_' + selector).append('<option value = '+ user.id +'>' + user.name +'</option>')
-        });
-  });
-});
+    $('#search_product').click(function () {
+        var clave = $('#product_id').val();
+        var product = products.filter(p => p.clave == clave)[0];
+        if (!product) {
+            swal.fire({
+                title: 'Error',
+                text: 'Producto no encontrado!',
+                type: 'error',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Aceptar!'
+            });
+            return;
+        }
+        var exist = selectedProducts.filter(p => p == product.id);
+        if (exist.length) {
+            Swal.fire(
+                'No permitido',
+                'Este producto ya se ha agregado',
+                'error'
+            );
+            return;
+        }
 
-$(".usuarios").change(function(){
-      var id_user = $(this).val();
-      var selector = $(this).attr("alt");
-      $("#id_user_" + selector).val(id_user);
+        selectedProducts.push(product.id);
+        $('#product_id').val('');
+        if (user.type_user == 1) {
+            p_t.row.add([
+                product.clave,
+                product.description,
+                product.category.name,
+                product.branch.name,
+                product.line ? product.line.name : `<span class="text-center badge badge-primary">Pieza</span>`,
+                product.weigth ? product.weigth + ' gr' : 'N/A',
+                '$ ' + product.price,
+                `<button type="button" class="btn btn-danger remove" alt="${product.id}"><i class="icon fa-remove"></i>
+        </button>`
+            ]).draw(false);
+        } else {
+            p_t.row.add([
+                product.clave,
+                product.description,
+                product.category.name,
+                product.line ? product.line.name : `<span class="text-center badge badge-primary">Pieza</span>`,
+                product.weigth ? product.weigth + ' gr' : 'N/A',
+                '$ ' + product.price,
+                `<button type="button" class="btn btn-danger remove" alt="${product.id}"><i class="icon fa-remove"></i>
+                </button>`
+            ]).draw(false);
+        }
+
     });
 
-    $('#product').change(function() {
-        var clave = $(this).val();
-  console.log("===> clave", clave);
-  var p = products.filter(p => p.clave == clave)[0];
-  console.log(p);
-  $('#branch').val(p.branchName)
-  $('#branch_id').val(p.branchId)
-  $('#product_id').val(p.id);
-  let brancehesList = branches.filter(b => b.id != p.branchId);
+    $('#productsInTranfer').on('click', '.remove', function () {
+        var id = $(this).attr('alt');
+        var row = $(this).parents('tr');
+        var index = selectedProducts.map(p => p).indexOf(selectedProducts.filter(p => p == id)[0])
+        selectedProducts.splice(index, 1);
+        if ($(row).hasClass('child')) {
+            p_t.row($(row).prev('tr')).remove().draw();
+        }
+        else {
+            p_t.row($(this).parents('tr')).remove().draw();
+        }
+    });
 
-  $('#branches').empty();
-  brancehesList.forEach(element => {
-    $('#branches').append(new Option(element.name, element.id, true, true));
-  });
+    $('#submit').click(function (e) {
+        if ($('#form')[0].checkValidity()) {
+            $('#products').val(JSON.stringify(selectedProducts));
+            if (selectedProducts.length < 1) {
+                Swal.fire(
+                    'No permitido',
+                    'Debes agregar por lo menos un producto',
+                    'error'
+                );
+                return;
+            }
+            $('#form').submit();
+        }
+    });
 
-});
-
-</script>
-@endsection
-<!-- END Función-->
-@section('traspaso')
-<script type="text/javascript">
-    $('.accept').click(function(){
-  var id = $(this).attr('alt');
-  $('#t_product_id').val(id);
-  $('#status').val(1)
-
-  $('#form').submit();
-});
 </script>
 @endsection
