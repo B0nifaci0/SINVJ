c56cd78abcfaf2c2138e6739118ccfea6f5f2a38
Irving <irvingrodrigues11@gmail.com>
2020-10-29 10:14:33 -0600

    diseño corte diario

--- resources/views/Branches/boxcut/dailycashcut.blade.php
+++ resources/views/Branches/boxcut/dailycashcut.blade.php
@@ -13,30 +13,36 @@
         <img align="left" width="220px" height="100px" src="{{ $shop->image }}"></br>
         <p align="right">Fecha: {{$branch-> date}}</p>
         <p align="right">Hora: {{$branch-> hour}}</p>
-        <h3 align="center">Corte Por Sucursal: {{$branch ->name}}</h3>
+        <h3 align="center">Corte de caja diario</h3>
+        <h3>Sucursal: {{$branch ->name}}</h3>
       </div>
         </br>
         <div class="panel">
               <table class="table table-hover dataTable table-striped w-full" data-plugin="dataTable">
         <tr>
           <th>VENTA TOTAL (TARJETA + EFECTIVO)</th>
-          <td>$ {{$branch->total }} </td>
+          <tr>
+          <td>$ {{$branch->total }} </td></tr>
         </tr>
         <tr>
           <th>VENTA TARJETA</th>
-          <td> $ {{$branch->tarjeta }} </td>
+          <tr>
+          <td> $ {{$branch->tarjeta }} </td></tr>
         </tr>
         <tr>
           <th>VENTA EFECTIVO</th>
-          <td>$ {{$branch->efectivo }} </td>
+          <tr>
+          <td>$ {{$branch->efectivo }} </td></tr>
         </tr>
         <tr>
           <th>GASTOS SUCURSAL</th>
-          <td>$ {{$branch->gastos }} </td>
+          <tr>
+          <td>$ {{$branch->gastos }} </td></tr>
         </tr>
         <tr>
           <th> TOTAL EFECTIVO</th>
-          <td>$ {{$branch->totalFin }} </td>
+          <tr>
+          <td>$ {{$branch->totalFin }} </td></tr>
         </tr>
       </table>
 
