70d90576f01fc332b091f52fef089765e4abe729
Pedro <pether.1994@gmail.com>
2020-11-22 14:00:10 -0600

    index eliminr clientes

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,18 +1,6 @@
-@extends('layout.layoutdas')
-@section('title')
-LISTA DE SUCURSALES
-@endsection
-
-@section('admin-section')
-@endsection
-
-@section('nav')
-
-@endsection
-@section('menu')
-
-@endsection
-@section('content')
+@extends('layout.layoutdas') @section('title') LISTA DE SUCURSALES @endsection
+@section('admin-section') @endsection @section('nav') @endsection
+@section('menu') @endsection @section('content')
 <div class="panel-body">
   @if (session('mesage'))
   <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -21,16 +9,14 @@ LISTA DE SUCURSALES
       <span aria-hidden="true">&times;</span>
     </button>
   </div>
-  @endif
-  @if (session('mesage-update'))
+  @endif @if (session('mesage-update'))
   <div class="alert alert-warning alert-dismissible fade show" role="alert">
     <strong>{{ session('mesage-update') }}</strong>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
   </div>
-  @endif
-  @if (session('mesage-delete'))
+  @endif @if (session('mesage-delete'))
   <div class="alert alert-danger alert-dismissible fade show" role="alert">
     <strong>{{ session('mesage-delete') }}</strong>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -47,12 +33,12 @@ LISTA DE SUCURSALES
           <div class="panel-actions float-right">
             <div class="container-fluid row float-right">
               @if(Auth::user()->type_user == 1 )
-              <!-- Botón para Generar PDF de productos-->
               <div class="col-6">
                 <button onclick="window.location.href='/clientes/create'" type="button"
-                  class="btn btn-sm small btn-floating  toggler-left  btn-info waves-effect waves-light waves-round float-left "
+                  class="btn btn-sm small btn-floating toggler-left btn-info waves-effect waves-light waves-round float-left"
                   data-toggle="tooltip" data-original-title="Nuevo cliente">
-                  <i class="icon md-plus" aria-hidden="true"></i></button>
+                  <i class="icon md-plus" aria-hidden="true"></i>
+                </button>
               </div>
               <!-- END Botón-->
               @endif
@@ -64,18 +50,21 @@ LISTA DE SUCURSALES
         <div class="example-wrap">
           <div class="nav-tabs-horizontal" data-plugin="tabs">
             <ul class="nav nav-tabs" role="tablist">
-              <li class="nav-item" role="presentation"><a class="nav-link active" data-toggle="tab"
-                  href="#exampleTabsOne" aria-controls="exampleTabsOne" role="tab">Menudeo</a></li>
-              <li class="nav-item" role="presentation"><a class="nav-link" data-toggle="tab" href="#exampleTabsTwo"
-                  aria-controls="exampleTabsTwo" role="tab">Mayoristas</a></li>
+              <li class="nav-item" role="presentation">
+                <a class="nav-link active" data-toggle="tab" href="#exampleTabsOne" aria-controls="exampleTabsOne"
+                  role="tab">Menudeo</a>
+              </li>
+              <li class="nav-item" role="presentation">
+                <a class="nav-link" data-toggle="tab" href="#exampleTabsTwo" aria-controls="exampleTabsTwo"
+                  role="tab">Mayoristas</a>
+              </li>
             </ul>
             <div class="tab-content">
               <div class="tab-pane active" id="exampleTabsOne" role="tabpanel">
                 <div class="page-content panel-body container-fluid">
-                  <table id="public" class=" display table table-hover dataTable table-striped w-full"
-                    data-plugin="dataTable">
+                  <table id="retail" class="table display responsive nowrap" style="width: 100%">
+                    {{ csrf_field() }}
                     <thead>
-                      {{ csrf_field() }}
                       <tr>
                         <th>Nombre</th>
                         <th>Teléfono</th>
@@ -83,52 +72,15 @@ LISTA DE SUCURSALES
                         <th>Opciones</th>
                       </tr>
                     </thead>
-                    <tbody>
-                      @foreach ($public as $client)
-                      <tr id="row{{ $client->id }}">
-                        <td>{{$client->name}}</td>
-                        <td>
-                          @if($client->phone_number == null || $client->phone_number == 0000000000)
-                          Sin Telefono
-                          @else
-                          {{$client->phone_number }}
-                          @endif
-                        </td>
-                        <td>{{$client->branch ? $client->branch->name : 'Sin sucursal' }}</td>
-                        <td>
-                          <!-- Botón para ver cliente-->
-                          <a href="/clientes/{{ $client->id }}" type="button"
-                            class="btn btn-icon btn-primary waves-effect waves-light waves-round" data-toggle="tooltip"
-                            data-original-title="Ver Perfil Cliente">
-                            <i class="icon fa-search" aria-hidden="true"></i></button></a>
-                          <!--END Botón -->
-                          <!-- Botón para editar cliente-->
-                          <a href="/clientes/{{$client->id}}/edit"> <button type="button"
-                              class="btn btn-icon btn-info waves-effect waves-light waves-round" data-toggle="tooltip"
-                              data-original-title="Editar Cliente">
-                              <i class=" icon md-edit" aria-hidden="true"></i></button></a>
-                          <!--END Botón -->
-                          <!-- Botón para eliminar cliente -->
-                          <button class="btn btn-icon btn-danger waves-effect waves-light waves-round delete"
-                            alt="{{$client->id}}" role="button" data-toggle="tooltip" data-original-title="Eliminar">
-                            <i class="icon md-delete" aria-hidden="true"></i></button></a>
-                          <!--END Botón -->
-                        </td>
-                      </tr>
-                      @endforeach
-                    </tbody>
                   </table>
-                  <!-- END Table-->
                 </div>
               </div>
-
             </div>
             <div class="tab-pane" id="exampleTabsTwo" role="tabpanel">
               <div class="page-content panel-body container-fluid">
-                <!-- Tabla para listar productos-->
-                <table id="wholesaler" class="table table-hover dataTable table-striped w-full" data-plugin="dataTable">
+                <table id="wholesale" class="table display responsive nowrap" style="width: 100%">
+                  {{ csrf_field() }}
                   <thead>
-                    {{ csrf_field() }}
                     <tr>
                       <th>Nombre</th>
                       <th>Primer apellido</th>
@@ -139,124 +91,159 @@ LISTA DE SUCURSALES
                       <th>Opciones</th>
                     </tr>
                   </thead>
-                  <tfoot>
-                    <tr>
-                      <th>Nombre</th>
-                      <th>Primer apellido</th>
-                      <th>Segundo apellido</th>
-                      <th>Teléfono</th>
-                      <th>Límite de Crédito</th>
-                      <th>Sucursal</th>
-                      <th>Opciones</th>
-                    </tr>
-                  </tfoot>
-                  <tbody>
-                    @foreach ($wholesaler as $client)
-                    <tr id="row{{ $client->id }}">
-                      <td>{{$client->name}}</td>
-                      <td>{{$client->first_lastname }}</td>
-                      <td>{{$client->second_lastname }}</td>
-                      <td>{{$client->phone_number }}</td>
-                      <td>{{$client->credit}}</td>
-                      <td>{{$client->branch ? $client->branch->name : 'Sin sucursal' }}</td>
-                      <td>
-                        <!-- Botón para ver cliente-->
-                        <a href="/clientes/{{ $client->id }}" type="button"
-                          class="btn btn-icon btn-primary waves-effect waves-light waves-round" data-toggle="tooltip"
-                          data-original-title="Ver Perfil Cliente">
-                          <i class="icon fa-search" aria-hidden="true"></i></button></a>
-                        <!--END Botón -->
-                        <!-- Botón para editar cliente-->
-                        <a href="/clientes/{{$client->id}}/edit"> <button type="button"
-                            class="btn btn-icon btn-info waves-effect waves-light waves-round" data-toggle="tooltip"
-                            data-original-title="Editar Cliente">
-                            <i class=" icon md-edit" aria-hidden="true"></i></button></a>
-                        <!--END Botón -->
-                        <!-- Botón para eliminar cliente -->
-                        <button class="btn btn-icon btn-danger waves-effect waves-light waves-round delete"
-                          alt="{{$client->id}}" role="button" data-toggle="tooltip" data-original-title="Eliminar">
-                          <i class="icon md-delete" aria-hidden="true"></i></button></a>
-                        <!--END Botón -->
-                      </td>
-                    </tr>
-                    @endforeach
-                  </tbody>
                 </table>
-                <!-- END Table-->
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <!-- End Panel Basic -->
-    @endsection
-    @section('barcode-product')
+    @endsection @section('barcode-product')
     <script type="text/javascript">
-      $(document).ready(function(){
-        $('#table').DataTable({
-            retrieve: true,
-            //responsive: true,
-            //paging: false,
-            //searching: false
-        });
- 
-    });
-    </script>
-    @endsection
-    <!-- Función Sweet Alert para eliminar sucursal-->
-    @section('delete-sucursales')
-    <script type="text/javascript">
-      console.log("a")
-$(document).ready(function() {
-  console.log("b")
-  $(".delete").click(function() {
-    var id = $(this).attr("alt");
-    console.log(id);
-    Swal.fire({
-      title: 'Confirmación',
-      text: "¿Seguro que desea eliminar este registro?",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#d33' ,
-      cancelButtonColor: '#3085d6',
-      confirmButtonText: 'Si, Borralo!'
-    }).then((result) => {
-      if (result.value)
-      {
-        $.ajaxSetup({
-         headers: {
-             'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-          }
-        });
-        $.ajax({
-          url:  '/sucursales/' + id,
-          method: 'DELETE',
-          success: function () {
-            $("#row" + id).remove();
-            Swal.fire(
-              'Eliminado',
-              'El registro ha sido eliminado con éxito.',
-              'success'
-            )
+      var user = {!! $user !!};
+
+      console.log("usuario= " + user.id)
+
+      $(document).ready(function () {
+        retail = $("#retail").DataTable({
+          serverSide: true,
+          pagingType: "simple",
+          language: {
+            url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
           },
-          error: function () {
-            Swal.fire(
-              'Eliminado',
-              'El registro no ha sido eliminado.'+ id,
-              'error'
-            )
+          ajax: "clients/retail",
+          columns: [
+            { data: "name" },
+            {
+              data: "phone_number",
+              render: function (data) {
+                if (data) return data;
+                else return "Sin Numero";
+              },
+            },
+            {
+              data: "clients.branch.name",
+              render: function (data) {
+                if (data) return data;
+                else return "N/A";
+              },
+            },
+            {
+              data: "id",
+              render: (data, type, row, meta) => {
+                return `<a type="button" href="/clientes/` + data + `"
+              class="btn btn-icon btn-primary waves-effect waves-light waves-round" data-toggle="tooltip"
+              data-original-title="Info producto">
+              <i class="icon fa-search" aria-hidden="true"></i></a>
+            <a type="button" href="/clientes/`+ data + `/edit" class="btn btn-icon btn-info waves-effect waves-light waves-round"
+              data-toggle="tooltip" data-original-title="Editar">
+              <i class="icon md-edit" aria-hidden="true"></i></a>
+            <button class="btn btn-icon btn-danger waves-effect waves-light waves-round delete" alt="`+ data + `" role="button"
+              data-toggle="tooltip" data-original-title="Borrar">
+              <i class="icon md-delete" aria-hidden="true"></i>
+            </button>`;
+              }
+            },
+          ],
+        });
+
+        wholesale = $("#wholesale").DataTable({
+          serverSide: true,
+          pagingType: "simple",
+          language: {
+            url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+          },
+          ajax: "clients/wholesale",
+          columns: [
+            { data: "name" },
+            { data: "first_lastname" },
+            { data: "second_lastname" },
+            { data: "phone_number" },
+            { data: "positive_balance" },
+            {
+              data: "clients.branch.name",
+              render: function (data) {
+                if (data) return data;
+                else return "N/A";
+              },
+            },
+            {
+              data: "id",
+              render: (data, type, row, meta) => {
+                return `<a type="button" href="/clientes/` + data + `"
+                class="btn btn-icon btn-primary waves-effect waves-light waves-round" data-toggle="tooltip"
+                data-original-title="Info producto">
+                <i class="icon fa-search" aria-hidden="true"></i></a>
+              <a type="button" href="/clientes/`+ data + `/edit" class="btn btn-icon btn-info waves-effect waves-light waves-round"
+                data-toggle="tooltip" data-original-title="Editar">
+                <i class="icon md-edit" aria-hidden="true"></i></a>
+              <button class="btn btn-icon btn-danger waves-effect waves-light waves-round delete" alt="`+ data + `" role="button"
+                data-toggle="tooltip" data-original-title="Borrar">
+                <i class="icon md-delete" aria-hidden="true"></i>
+              </button>`;
+              }
+            },
+          ],
+        });
+
+        $('a[data-toggle="tab"]').on("shown.bs.tab", function (e) {
+          $($.fn.dataTable.tables(true))
+            .DataTable()
+            .columns.adjust()
+            .responsive.recalc();
+        });
+      });
+
+      $(document).on('click', '.delete', function () {
+        var id = $(this).attr("alt");
+        console.log(id);
+        Swal.fire({
+          title: 'Confirmación',
+          text: "¿Seguro que desea eliminar este registro?",
+          type: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Si, Borralo!'
+        }).then((result) => {
+          if (result.value) {
+            $.ajaxSetup({
+              headers: {
+                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+              }
+            });
+            $.ajax({
+              url: '/clientes/' + id,
+              method: 'DELETE',
+              success: function (response) {
+                if (response.success) {
+                  retail.ajax.reload()
+                  wholesale.ajax.reload()
+                  Swal.fire(
+                    'Eliminado',
+                    'El registro ha sido eliminado.',
+                    'success'
+                  )
+                } else {
+                    Swal.fire(
+                      'No Eliminado',
+                      'El cliente no ha sido eliminado porqué esta activo en una venta',
+                      'error'
+                    ) 
+                }
+              },
+              error: function () {
+                Swal.fire(
+                  'Eliminado',
+                  'El registro no ha sido eliminado.',
+                  'error'
+                )
+              }
+            })
           }
         })
-      }
-    })
-
-  });
-
-  $('#table').DataTable();
-});
-
-
+      });
     </script>
     @endsection
-    <!--END Función-->
+  </div>
+</div>
